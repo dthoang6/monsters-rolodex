fdecc4c770031aca6cc6d41dd57354c56509fb0f
Your Name <dthoang6@gmail.com>
2023-04-24 15:28:08 -0700

    staring monsters rolodex project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
+import "./App.css"
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    }
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users =>
+        this.setState(
+          () => {
+            return { monsters: users }
+          },
+          () => {
+            console.log(this.state)
+          }
+        )
+      )
+  }
+  render() {
+    const filteredMonsters = this.state.monsters.filter(item => {
+      return item.name.toLocaleLowerCase().includes(this.state.searchField)
+    })
+
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="searching monsters"
+          onChange={event => {
+            const searchField = event.target.value.toLocaleLowerCase()
+
+            this.setState(() => {
+              return { searchField }
+            })
+          }}
+        />
+
+        {filteredMonsters.map(item => {
+          return <h1 key={item.id}>{item.name}</h1>
+        })}
+      </div>
+    )
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +59,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Hello, my name is tom hoang.
         </a>
       </header>
     </div>
-  );
-}
+  )
+} */
 
-export default App;
+export default App
